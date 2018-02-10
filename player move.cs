822ef37412484acc06e4fa6db1bd108dd8d1c6d1
Yvonne J <33183509+Yvojohn5@users.noreply.github.com>
2018-02-10 10:25:07 -0600

    The updated files
    There are duplicates but I tried my best. >-<;;

---
+++ player move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour {
+
+    private float speed;
+
+    private Vector2 direction;
+    // Use this for initialization
+    void Start() {
+        direction = Vector2.up;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+    }
+    public void Move()
+    {
+        transform.Translate(direction*speed*Time.deltaTime);
+
+    }
+}
