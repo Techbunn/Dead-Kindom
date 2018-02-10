0a35cb98695f8369553f96b2e00bdd3614375c6e
Yvonne J <33183509+Yvojohn5@users.noreply.github.com>
2018-02-10 01:42:56 -0600

    Here is a sample of the unity stuff

---
+++ Player script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+
+    [RequireComponent(typeof(Rigidbody2D))]
+
+    public class PlayerScript : MonoBehaviour
+    {
+
+        public float playerSpeed = 4f
+        void Start()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+            Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
+        }
+    }
+}
